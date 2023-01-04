7f1f151a21499da98beee5e0878ecb206e77bc22
lexie70 <alexandracampos658@gmail.com>
2023-01-04 23:42:58 +0000

    parte estetica terminada

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,17 +1,18 @@
+import { func } from "prop-types";
 import  React ,{ useState } from "react";
 
-const colors =["red","yellow","green"]
-
 function TrafficLight(){
-    const [color,setColor]= useState("btn btn-danger");
-   
-    return(
-    <div className="btn-group-vertical "  >
-    <button type="button" className="btn btn-danger"></button>
-    <button type="button" className="btn btn-warning"></button>
-    <button type="button" className="btn btn-success"></button>
-  </div>
+    const [color,setColor]= useState("");
 
-    )
-}
+      
+      return(<div className="bg-dark btn-group-vertical h-100 d-inline-block ">
+    <div className="btn-group-vertical "  >
+    <button type="button" style={{height: '100px', width: '100px'}} className={"btn btn-danger rounded-circle ms-3 me-3 mt-3"+ (color === "rojo"? "shadow":"" )} onClick={()=>{setColor("rojo")}}></button>
+    <button type="button" style={{height: '100px', width: '100px'}} className={"btn btn-warning rounded-circle ms-3 me-3 " + (color === "amarillo"? "shadow":"") } onClick={()=>{setColor("amarillo")}}></button>
+    <button type="button" style={{height: '100px', width: '100px'}} className={"btn btn-success rounded-circle ms-3 me-3 mb-3"+ (color === "verde"?"shadow":"") } onClick={()=>{setColor("verde")}}></button>
+  </div>
+  </div>
+    );
+  };
+
 export default TrafficLight;
