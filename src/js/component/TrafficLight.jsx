d39ceae10bbe64f98007776b549695d36950f99d
lexie70 <alexandracampos658@gmail.com>
2023-01-11 04:27:15 +0000

    fixing

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -5,11 +5,11 @@ function TrafficLight(){
     const [color,setColor]= useState("");
 
       
-      return(<div className="bg-dark btn-group-vertical h-100 d-inline-block ">
+      return(<div className="bg-dark btn-group-vertical h-100 d-inline-block">
     <div className="btn-group-vertical "  >
-    <button type="button"  className={"btn btn-danger rounded-circle ms-3 me-3 mt-3"+ (color === "rojo"? "shadow-lg" : "" )} style={{height: '100px', width: '100px'}} onClick={()=>{setColor("rojo")}}></button>
-    <button type="button" className={"btn btn-warning rounded-circle ms-3 me-3 " + (color === "amarillo"? "shadow-lg": "") } style={{height: '100px', width: '100px'}} onClick={()=>{setColor("amarillo")}}></button>
-    <button type="button"  className={"btn btn-success rounded-circle ms-3 me-3 mb-3"+ (color === "verde"? "shadow-lg" : "") }style={{height: '100px', width: '100px'}} onClick={()=>{setColor("verde")}}></button>
+    <button type="button"  className={"btn btn-danger rounded-circle ms-3 me-3 mt-3"+ (color === "rojo"? "shadow" : "" )} style={{height: '100px', width: '100px'}} onClick={()=>{setColor("rojo")}}></button>
+    <button type="button" className={"btn btn-warning rounded-circle ms-3 me-3 " + (color === "amarillo"? "shadow": "") } style={{height: '100px', width: '100px'}} onClick={()=>{setColor("amarillo")}}></button>
+    <button type="button"  className={"btn btn-success rounded-circle ms-3 me-3 mb-3"+ (color === "verde"? "shadow" : "") }style={{height: '100px', width: '100px'}} onClick={()=>{setColor("verde")}}></button>
   </div>
   </div>
     );
