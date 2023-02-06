8670aff3764c6054cc10e5aceef77064b8601d22
lexie70 <alexandracampos658@gmail.com>
2023-02-06 19:28:08 +0000

    classname change

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -6,7 +6,7 @@ function TrafficLight(){
 
       
       return(<div className="bg-dark btn-group-vertical h-100 d-inline-block">
-    <div className="btn-group-vertical "  >
+    <div className="btn-group-vertical"  >
     <button type="button"  className={"btn btn-danger rounded-circle ms-3 me-3 mt-3"+ (color === "rojo"? "shadow" : "" )} style={{height: '100px', width: '100px'}} onClick={()=>{setColor("rojo")}}></button>
     <button type="button" className={"btn btn-warning rounded-circle ms-3 me-3 " + (color === "amarillo"? "shadow": "") } style={{height: '100px', width: '100px'}} onClick={()=>{setColor("amarillo")}}></button>
     <button type="button"  className={"btn btn-success rounded-circle ms-3 me-3 mb-3"+ (color === "verde"? "shadow" : "") }style={{height: '100px', width: '100px'}} onClick={()=>{setColor("verde")}}></button>
